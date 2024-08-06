5f27414e133df066ec0851806e6f8ec26a3fc111
KOLE_VIKAS <165263344+GHOSTs-OCTOPUS@users.noreply.github.com>
2024-08-06 05:04:11 +0000

    Time: 111 ms (60.39%), Space: 15.6 MB (18.36%) - LeetHub

---
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def __init__(self, nums):
+        self.array = nums
+        self.original = list(nums)
+
+    def reset(self):
+        self.array = self.original
+        self.original = list(self.original)
+        return self.array
+
+    def shuffle(self):
+        aux = list(self.array)
+
+        for idx in range(len(self.array)):
+            remove_idx = random.randrange(len(aux))
+            self.array[idx] = aux.pop(remove_idx)
+
+        return self.array
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
