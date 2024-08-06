7212a35cac9a314d28eb0ef3ef1d10d2b2d0bf43
KOLE_VIKAS <165263344+GHOSTs-OCTOPUS@users.noreply.github.com>
2024-08-06 04:56:26 +0000

    Time: 36 ms (33.23%), Space: 15.6 MB (20.81%) - LeetHub

---
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long int ans=0;
+        int one=0;
+        int n=s.length();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='1'){
+                ans=ans+(n-i-1-one);
+                one++;
+            }
+        }
+        return ans;
+    }
+};
