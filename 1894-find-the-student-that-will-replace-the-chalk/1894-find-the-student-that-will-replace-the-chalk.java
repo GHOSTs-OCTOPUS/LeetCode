a453ab028cafe9e0348d159239d97e181fec430a
KOLE_VIKAS <165263344+GHOSTs-OCTOPUS@users.noreply.github.com>
2024-09-02 08:20:05 +0000

    Time: 2 ms (72.59%), Space: 59.9 MB (60.59%) - LeetHub

---
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int chalkReplacer(int[] chalk, int k) {
+        // Find the sum of all elements.
+        long sum = 0;
+        for (int i = 0; i < chalk.length; i++) {
+            sum += chalk[i];
+            if (sum > k) {
+                break;
+            }
+        }
+        // Find modulo of k with sum.
+        k = k % (int) sum;
+        for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k = k - chalk[i];
+        }
+        return 0;
+    }
+}
