ba2fa3dffc40a088664d13b1145c45bbac37dfe3
KOLE_VIKAS <165263344+GHOSTs-OCTOPUS@users.noreply.github.com>
2024-10-30 10:20:12 +0530

    Time: 63 ms (45.45%), Space: 14.9 MB (43.99%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        int N = nums.size();
+
+        vector<int> lisLength(N, 1);
+        vector<int> ldsLength(N, 1);
+
+        // Stores the length of longest increasing subsequence that ends at i.
+        for (int i = 0; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[i] > nums[j]) {
+                    lisLength[i] = max(lisLength[i], lisLength[j] + 1);
+                }
+            }
+        }
+
+        // Stores the length of longest decreasing subsequence that starts at i.
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = i + 1; j < N; j++) {
+                if (nums[i] > nums[j]) {
+                    ldsLength[i] = max(ldsLength[i], ldsLength[j] + 1);
+                }
+            }
+        }
+
+        int minRemovals = INT_MAX;
+        for (int i = 0; i < N; i++) {
+            if (lisLength[i] > 1 && ldsLength[i] > 1) {
+                minRemovals =
+                    min(minRemovals, N - lisLength[i] - ldsLength[i] + 1);
+            }
+        }
+
+        return minRemovals;
+    }
+};
