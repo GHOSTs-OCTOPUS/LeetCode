78e09948d9e73f417bad71da983f4c79bf0d5029
KOLE_VIKAS <165263344+GHOSTs-OCTOPUS@users.noreply.github.com>
2025-07-13 16:34:42 +0530

    Time: 97 ms (51.58%), Space: 67.5 MB (75.79%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} players
+ * @param {number[]} trainers
+ * @return {number}
+ */
+const matchPlayersAndTrainers = (p, t) => {
+    p.sort((a, b) => a - b);
+    t.sort((a, b) => a - b);
+    let i = 0, j = 0, c = 0;
+    while (i < p.length && j < t.length) c += p[i] <= t[j++] ? ++i && 1 : 0;
+    return c;
+    
+};
