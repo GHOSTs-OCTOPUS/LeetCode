db690014657a581c5560d48e5ee014952930743b
KOLE_VIKAS <vikaskole786@gmail.com>
2025-10-20 13:06:39 +0530

    Time: 1 ms (50.59%), Space: 57 MB (8.55%) - LeetHub

---
+++ 2137-final-value-of-variable-after-performing-operations/2137-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function (operations) {
+    let x = 0;
+    for (const op of operations) {
+        if ("X++" === op || "++X" === op) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    return x;
+};
