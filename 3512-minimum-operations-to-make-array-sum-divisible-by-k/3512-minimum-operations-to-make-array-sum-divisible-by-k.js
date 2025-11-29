f131a6d14662de5390e879920129bc75ab7ad98f
KOLE_VIKAS <vikaskole786@gmail.com>
2025-11-29 10:50:45 +0530

    Time: 1 ms (78.24%), Space: 56.9 MB (64.12%) - LeetHub

---
+++ 3512-minimum-operations-to-make-array-sum-divisible-by-k/3512-minimum-operations-to-make-array-sum-divisible-by-k.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var minOperations = function(nums, k) {
+    let sum = 0;
+    for (let num of nums) sum += num;
+    return sum % k;
+};
