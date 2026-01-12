b21b1e21b97423f8da73bc0ffd7a1f5ebc6b8ea9
KOLE_VIKAS <vikaskole786@gmail.com>
2026-01-12 14:09:50 +0530

    Time: 0 ms (100%), Space: 57.2 MB (18.28%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var minTimeToVisitAllPoints = function(points) {
+    ans = 0;
+    for(i = 1; i < points.length; i++) {
+        ans += Math.max(Math.abs(points[i][0] - points[i - 1][0]), Math.abs(points[i][1] - points[i - 1][1]));
+    }
+    return ans;
+};
