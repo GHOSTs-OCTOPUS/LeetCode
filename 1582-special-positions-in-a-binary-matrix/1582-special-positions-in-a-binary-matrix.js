a3ff0155c639dd41b9a906efac938b57152ee631
KOLE_VIKAS <vikaskole786@gmail.com>
2026-03-04 11:00:27 +0530

    Time: 4 ms (40%), Space: 57.5 MB (25.71%) - LeetHub

---
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function(mat) {
+    const m = mat.length, n = mat[0].length;
+    const row = new Array(m).fill(0);
+    const col = new Array(n).fill(0);
+    
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1) {
+                row[i]++;
+                col[j]++;
+            }
+        }
+    }
+    
+    let ans = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (mat[i][j] === 1 && row[i] === 1 && col[j] === 1) {
+                ans++;
+            }
+        }
+    }
+    
+    return ans;
+};
