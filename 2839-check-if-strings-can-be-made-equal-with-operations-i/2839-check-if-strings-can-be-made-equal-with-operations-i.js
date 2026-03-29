9a8722edd494778bd7a63a5dabf454ff1ef83ef6
KOLE_VIKAS <vikaskole786@gmail.com>
2026-03-29 14:22:55 +0530

    Time: 1 ms (57.14%), Space: 58.2 MB (7.14%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    return ([s1[0], s1[2]].sort().join('') === [s2[0], s2[2]].sort().join('')) &&
+           ([s1[1], s1[3]].sort().join('') === [s2[1], s2[3]].sort().join(''));
+};
