62a021a9b4c49692f27a89783ae7f4daa80df44c
KOLE_VIKAS <vikaskole786@gmail.com>
2026-04-19 12:03:26 +0530

    Time: 4 ms (13.33%), Space: 68.4 MB (20%) - LeetHub

---
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var maxDistance = function(nums1, nums2) {
+    let i = 0;
+    let j = 0;
+    let ans = 0;
+
+    while (i < nums1.length && j < nums2.length) {
+
+        if (i > j) {
+            j++;
+            continue;
+        }
+
+        if (nums1[i] <= nums2[j]) {
+            ans = Math.max(ans, j - i);
+            j++;
+        } else {
+            i++;
+        }
+    }
+
+    return ans;
+};
