2b857717d999e0fbd6c7f7cc49abc94124dcaa9b
KOLE_VIKAS <vikaskole786@gmail.com>
2026-05-31 10:34:04 +0530

    Time: 93 ms (56.14%), Space: 75.9 MB (8.77%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} mass
+ * @param {number[]} asteroids
+ * @return {boolean}
+ */
+var asteroidsDestroyed = function(mass, asteroids) {
+    asteroids.sort((a, b) => a - b);
+
+    for (const asteroid of asteroids) {
+        if (mass < asteroid) {
+            return false;
+        }
+
+        mass += asteroid;
+    }
+
+    return true;
+};
