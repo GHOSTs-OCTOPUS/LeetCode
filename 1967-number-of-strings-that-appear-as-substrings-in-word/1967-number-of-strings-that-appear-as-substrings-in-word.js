1c19563801ffdd7d69cd495f7d9808fd3580e017
KOLE_VIKAS <vikaskole786@gmail.com>
2026-06-29 09:55:32 +0530

    Time: 0 ms (100%), Space: 53.4 MB (87.5%) - LeetHub

---
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} patterns
+ * @param {string} word
+ * @return {number}
+ */
+var numOfStrings = function(patterns, word) {
+
+    let count = 0;
+
+    for (const pattern of patterns) {
+
+        if (word.includes(pattern)) {
+            count++;
+        }
+    }
+
+    return count;
+};
