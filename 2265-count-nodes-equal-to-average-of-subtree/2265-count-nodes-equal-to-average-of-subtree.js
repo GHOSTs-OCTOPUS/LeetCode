7c2030d781f45a61e8b25f070bc22c10db28a9cc
KOLE_VIKAS <vikaskole786@gmail.com>
2026-09-10 11:15:35 +0530

    Time: 41 ms (100%), Space: 61.5 MB (11.43%) - LeetHub

---
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var averageOfSubtree = function(root) {
+    let answer = 0;
+
+    function dfs(node) {
+        if (node === null) {
+            return [0, 0];
+        }
+
+        const [leftSum, leftCount] = dfs(node.left);
+        const [rightSum, rightCount] = dfs(node.right);
+
+        const sum = leftSum + rightSum + node.val;
+        const count = leftCount + rightCount + 1;
+
+        if (node.val === Math.floor(sum / count)) {
+            answer++;
+        }
+
+        return [sum, count];
+    }
+
+    dfs(root);
+    return answer;
+};
